0449f66b7f8803724a913663e85005281aaf9177
tuonglua <tuonglua@stud.ntnu.no>
2017-05-01 21:52:37 +0200

    localhost alias

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 
+// localhost = http://127.0.0.1:8080/
+
 const API_KEY = 'AIzaSyDE8WguX7HpEvAy-_wtl4anoP1MF7-3Usc';
 
 class App extends Component{
