cc1135f915e688889e348c0a7dcb4c21d54f28cd
tuonglua <tuonglua@stud.ntnu.no>
2017-09-19 22:38:53 +0200

    Stuff

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+  const imageURL = video.snippet.thumbnails.default.url;
+  const titleURL = video.snippet.title;
+
   return (
-    <li>Video</li>
-  )
+    <li className='list-group-item'>
+      <div className='video-list media'>
+        <div className='media-left'>
+          <img className='media-object' src={ imageURL } />
+        </div>
+
+        <div className='media-body'>
+          <div className='media-heading'>
+            { titleURL }
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
