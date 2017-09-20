412244c495e66891f77e217fe02425bf21d09cc0
tuonglua <tuonglua@stud.ntnu.no>
2017-09-20 19:44:17 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 //Self-made omponents
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDE8WguX7HpEvAy-_wtl4anoP1MF7-3Usc';
 //Component
@@ -11,10 +12,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] }
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos }) //this.setState({ videos: videos})
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+       }) //this.setState({ videos: videos}) <=> this.setState({videos})
     });
   }
 
@@ -22,6 +29,7 @@ class App extends Component {
     return(
       <div>
         <SearchBar />
+        <VideoDetail video={ this.state.selectedVideo }/>
         <VideoList videos={ this.state.videos }/>
       </div>
     );
